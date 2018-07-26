7bb2b8dbcf4d2553d8cbb77aa131042c48df961a
Rajeshbezavada <41729529+Rajeshbezavada@users.noreply.github.com>
2018-07-26 06:51:14 -0400

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=3;
+		if(n>0)
+		{
+			System.out.println("positive");
+		}
+	}
+}
