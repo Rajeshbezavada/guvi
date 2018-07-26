af164a1eca4be09bd4c8249bac4828396d99947d
Rajeshbezavada <41729529+Rajeshbezavada@users.noreply.github.com>
2018-07-26 07:22:11 -0400

    Update positive.java

--- positive.java
+++ positive.java
@@ -1,6 +1,10 @@
+/* package whatever; // don't place package name! */
+
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
 class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
@@ -8,7 +12,14 @@ class Ideone
 		int n=3;
 		if(n>0)
 		{
-			System.out.println("positive");
+			System.out.println("Positive");
+		}
+		else if(n==0){
+				System.out.println("Zero");
+			
+		}
+		else{
+				System.out.println("Negative");
 		}
 	}
 }
