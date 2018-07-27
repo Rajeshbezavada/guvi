8d79a7e1f46d15ad95ade62d1e6be266866ea7c4
Rajeshbezavada <41729529+Rajeshbezavada@users.noreply.github.com>
2018-07-27 02:54:43 -0400

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Hello
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			System.out.println("Hello");
+		}
+		
+	
+}
+}
