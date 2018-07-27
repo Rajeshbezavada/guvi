924b365208abaa1a8a1d338a10f28135e92adac2
Rajeshbezavada <41729529+Rajeshbezavada@users.noreply.github.com>
2018-07-27 03:21:53 -0400

    Create Nintegers.java

---
+++ Nintegers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Nintegers
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int m=sc.nextInt();
+		int n=sc.nextInt();
+		int sum=0;
+		for(int i=1;i<=n;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.println(sum);
+}
+}
