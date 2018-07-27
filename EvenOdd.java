04420a5e948027d129c6c7d97cdf7512f9510e80
Rajeshbezavada <41729529+Rajeshbezavada@users.noreply.github.com>
2018-07-27 00:57:44 -0400

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class EvenOdd
+{
+public static void main(String[] args)
+{
+int n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+if(n%2==0)
+{
+System.out.println("Even");
+}
+else if((n%2)!=0 && n>0)
+{
+System.out.println("Odd");
+}
+else
+{
+System.out.println("Invalid");
+}
+}
