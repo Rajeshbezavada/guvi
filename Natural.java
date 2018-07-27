0e1e22877f0f17b68392e54bb9ac716abeffb456
Rajeshbezavada <41729529+Rajeshbezavada@users.noreply.github.com>
2018-07-27 03:12:45 -0400

    Create Natural.java

---
+++ Natural.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Natural
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int N=0;
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	for(int i=1;i<=n;i++)
+	{
+		N=(n*(n+1)/2);
+		
+	}
+	System.out.println(N);
+	
+}
+}
