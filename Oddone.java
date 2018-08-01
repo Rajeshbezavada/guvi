4cf27d71121142b51d02389ae33c54aafc7a8a6c
Rajeshbezavada <41729529+Rajeshbezavada@users.noreply.github.com>
2018-08-01 07:23:04 -0400

    Update Oddone.java

--- Oddone.java
+++ Oddone.java
@@ -8,7 +8,7 @@ class Oddone
 		Scanner sc=new Scanner(System.in);
 		int a=sc.nextInt();
 		int b=sc.nextInt();
-		for(int i=a;i<=b;i++)
+		for(int i=a+1;i<=b;i++)
 		{
 			if(i%2!=0)
 			System.out.println(i);
