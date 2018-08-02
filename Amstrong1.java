81946a24488e8d3891d007574540b03102b34b0d
Rajeshbezavada <41729529+Rajeshbezavada@users.noreply.github.com>
2018-08-02 07:20:22 +0000

    Create Amstrong1.java

---
+++ Amstrong1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+class Amstrong1{
+      public static void main(String aa[]){
+           int n,p,q;
+           int f;
+           int sum=0;
+           Scanner s=new Scanner(System.in);
+           p =s.nextInt();
+           n = s.nextInt();
+           for(int i=p;i<n;i++){
+                f=i;
+               int m=i;
+           while(m>0){
+               q=m%10;
+               sum=sum+q*q*q;
+               m=m/10;
+           }
+           if(f==sum){
+               System.out.printf("%d ",i);
+           }
+               sum=0;
+           }
+}
+}
