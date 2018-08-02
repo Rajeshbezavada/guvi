faaccc6ea035024516115913c161d6139a85b7ff
Rajeshbezavada <41729529+Rajeshbezavada@users.noreply.github.com>
2018-08-02 07:44:03 +0000

    Create Prime1.java

---
+++ Prime1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Prime1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        int low=sc.nextInt();
+        int high=sc.nextInt();
+
+        while (low < high) {
+            boolean flag = false;
+
+            for(int i = 2; i <= low/2; ++i) {
+                // condition for nonprime number
+                if(low % i == 0) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag)
+                System.out.print(low + " ");
+
+            ++low;
+        }
+    }
+}
